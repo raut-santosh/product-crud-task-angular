ec1eced2c49477e81936ff229f4afa820f121219
santosh raut <rautsantoshtukaram@gmail.com>
2023-12-17 11:15:38 +0530

    changed routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: 'products', loadChildren: () => import('./modules/product/product.module').then(m => m.ProductModule) },
+  { path: '', loadChildren: () => import('./modules/product/product.module').then(m => m.ProductModule) },
 ];
 
 @NgModule({
